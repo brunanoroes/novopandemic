b950d99ec2c2b1ca006706731a2dafaa3d983ffc
brunanoroes <143547466+brunanoroes@users.noreply.github.com>
2025-04-13 21:20:42 -0300

    refactor

--- components/CartaJogador.js
+++ components/CartaJogador.js
@@ -1,8 +1,7 @@
-Vue.component('cartajogador', {
+Vue.component('carta-jogador', {
   props: ['nome', 'funcao', 'imagem', 'habilidades'],
   template: `
     <div class="carta-jogador">
-      <img :src="imagem" alt="Imagem do Jogador" width="100" />
       <h3>{{ nome }} - {{ funcao }}</h3>
       <p>{{ habilidades.join(', ') }}</p>
     </div>
