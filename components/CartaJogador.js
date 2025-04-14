e6b5fcb50b4a19572e5fc1091f71161e28178bae
brunanoroes <143547466+brunanoroes@users.noreply.github.com>
2025-04-13 21:01:48 -0300

    ajustes

--- components/CartaJogador.js
+++ components/CartaJogador.js
@@ -1,15 +1,10 @@
 Vue.component('cartajogador', {
   props: ['nome', 'funcao', 'imagem', 'habilidades'],
-  methods: {
-    usarCarta() {
-      alert(`Carta de ${this.nome} foi usada!`);
-    },
-  },
   template: `
     <div class="carta-jogador">
-      <img :src="imagem" alt="Imagem do Jogador" />
-      <h2>{{ nome }} - {{ funcao }}</h2>
-      <p>{{ habilidades }}</p>
+      <img :src="imagem" alt="Imagem do Jogador" width="100" />
+      <h3>{{ nome }} - {{ funcao }}</h3>
+      <p>{{ habilidades.join(', ') }}</p>
     </div>
   `,
 });
