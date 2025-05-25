aee696798778853fb53d0e5366da26bdc7ae2b30
brunanoroes <143547466+brunanoroes@users.noreply.github.com>
2025-05-24 21:08:28 -0300

    Melhora a responsividade e a organização dos componentes: ajusta estilos para diferentes tamanhos de tela e refatora a estrutura do HTML para melhor alinhamento e espaçamento.

--- data/EspacosMarcadorSurto.js
+++ data/EspacosMarcadorSurto.js
@@ -1,56 +1,38 @@
 const espacosMarcadorSurtoJson = [
   {
     nome: '1',
-    x: 3,
-    y: 40,
     atual: true,
   },
   {
     nome: '2',
-    x: 6,
-    y: 45,
     atual: false,
   },
   {
     nome: '3',
-    x: 3,
-    y: 50,
     atual: false,
   },
   {
     nome: '4',
-    x: 6,
-    y: 55,
     atual: false,
   },
   {
     nome: '5',
-    x: 3,
-    y: 60,
     atual: false,
   },
   {
     nome: '6',
-    x: 6,
-    y: 65,
     atual: false,
   },
   {
     nome: '7',
-    x: 3,
-    y: 70,
     atual: false,
   },
   {
     nome: '8',
-    x: 6,
-    y: 75,
     atual: false,
   },
   {
     nome: 'X',
-    x: 3,
-    y: 80,
     atual: false,
   },
 ];
